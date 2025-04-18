879e479e98ea2dd2e88566bae665b1f4a98cbd1b
sknutsen <sondreknutsen1@gmail.com>
2025-04-18 10:00:52 +0200

    rewrite checkpoint
    - nvf nvim config

--- flake.nix
+++ flake.nix
@@ -3,38 +3,42 @@
 
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
-    nixgl.url = "github:nix-community/nixGL";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    
+    nvf = {
+      url = "github:notashelf/nvf";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, nixgl, home-manager, ... }@inputs:
+  outputs = { nixpkgs, home-manager, ... }@inputs:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
-        system = system;
-        overlays = [
-          (self: super: {
-            vulkan-validation-layers =
-              super.vulkan-validation-layers.overrideAttrs (oldAttrs: {
-                buildInputs = oldAttrs.buildInputs ++ [ super.spirv-tools ];
-              });
-          })
-          nixgl.overlay 
-        ];
+        inherit system;
       };
+      # pkgs = nixpkgs.legacyPackages.${system};
     in {
-      homeConfigurations.zdk = home-manager.lib.homeManagerConfiguration {
+      homeConfigurations."zdk" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
 
-        # extraSpecialArgs = { inherit inputs pkgs; };
+	extraSpecialArgs = {
+	  inherit inputs;
+          rootPath = ./.;
+	};
 
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
-        modules = [ ./home.nix ];
+        modules = [
+          inputs.nvf.homeManagerModules.default
+
+	  ./home.nix
+	];
 
         # Optionally use extraSpecialArgs
         # to pass through arguments to home.nix
--- home.nix
+++ home.nix
@@ -1,79 +1,207 @@
-{ config, pkgs, ... }:
-
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = "zdk";
-  home.homeDirectory = "/home/zdk";
+  config,
+  inputs,
+  pkgs,
+  ...
+}: {
+  home = {
+    # Home Manager needs a bit of information about you and the paths it should
+    # manage.
+    username = "zdk";
+    homeDirectory = "/home/zdk";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.05"; # Please read the comment before changing.
+    # The home.packages option allows you to install Nix packages into your
+    # environment.
+    packages = with pkgs; [
+      # # It is sometimes useful to fine-tune packages, for example, by applying
+      # # overrides. You can do that directly here, just don't forget the
+      # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+      # # fonts?
+      # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = with pkgs; [
-    nixgl.nixGLIntel
-    nixgl.nixVulkanIntel
-    # wezterm
-    zed-editor
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
+      # # You can also create simple shell scripts directly inside your
+      # # configuration. For example, this adds a command 'my-hello' to your
+      # # environment:
+      # (pkgs.writeShellScriptBin "my-hello" ''
+      #   echo "Hello, ${config.home.username}!"
+      # '')
+    ];
 
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+    # Home Manager is pretty good at managing dotfiles. The primary way to manage
+    # plain files is through 'home.file'.
+    file = {
+      # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+      # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+      # # symlink to the Nix store copy.
+      # ".screenrc".source = dotfiles/screenrc;
 
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
-  ];
+      # # You can also set the file content immediately.
+      # ".gradle/gradle.properties".text = ''
+      #   org.gradle.console=verbose
+      #   org.gradle.daemon.idletimeout=3600000
+      # '';
+    };
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
+    # Home Manager can also manage your environment variables through
+    # 'home.sessionVariables'. These will be explicitly sourced when using a
+    # shell provided by Home Manager. If you don't want to manage your shell
+    # through Home Manager then you have to manually source 'hm-session-vars.sh'
+    # located at either
+    #
+    #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+    #
+    # or
+    #
+    #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+    #
+    # or
+    #
+    #  /etc/profiles/per-user/zdk/etc/profile.d/hm-session-vars.sh
+    #
+    sessionVariables = {
+      EDITOR = "nvim";
+    };
 
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+    # This value determines the Home Manager release that your configuration is
+    # compatible with. This helps avoid breakage when a new Home Manager release
+    # introduces backwards incompatible changes.
+    #
+    # You should not change this value, even if you update Home Manager. If you do
+    # want to update the value, then make sure to first check the Home Manager
+    # release notes.
+    stateVersion = "24.05"; # Please read the comment before changing.
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/zdk/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = {
-    # EDITOR = "emacs";
-    NIXTESTENVVAR_LOL = "zdk";
+  programs.nvf = {
+    enable = true;
+    # your settings need to go into the settings attribute set
+    # most settings are documented in the appendix
+    settings = {
+      vim = {
+        viAlias = false;
+        vimAlias = true;
+        package = pkgs.neovim-unwrapped;
+
+        additionalRuntimePaths = [
+          # ./nvim
+          # (builtins.path {
+          #   path = rootPath + /nvim;
+          #   name = "nvim-runtime";
+          # })
+        ];
+
+        spellcheck = {
+          enable = true;
+        };
+
+        lsp = {
+          enable = true;
+          formatOnSave = true;
+          lspkind.enable = false;
+          lightbulb.enable = true;
+          lspsaga.enable = false;
+          trouble.enable = true;
+          lspSignature.enable = true;
+          otter-nvim.enable = false;
+          nvim-docs-view.enable = false;
+        };
+
+        # This section does not include a comprehensive list of available language modules.
+        # To list all available language module options, please visit the nvf manual.
+        languages = {
+          enableLSP = true;
+          enableFormat = true;
+          enableTreesitter = true;
+          enableExtraDiagnostics = true;
+
+          assembly.enable = false;
+          astro.enable = false;
+          bash.enable = true;
+          clang.enable = true;
+          csharp.enable = false;
+          css.enable = true;
+          dart.enable = false;
+          elixir.enable = false;
+          fsharp.enable = false;
+          gleam.enable = false;
+          go.enable = true;
+          haskell.enable = false;
+          html.enable = true;
+          java.enable = false;
+          julia.enable = false;
+          kotlin.enable = false;
+          lua.enable = true;
+          markdown.enable = true;
+          nix.enable = true;
+          nu.enable = false;
+          ocaml.enable = false;
+          python.enable = true;
+          r.enable = false;
+          ruby.enable = false;
+          rust = {
+            enable = false;
+            crates.enable = false;
+          };
+          scala.enable = false;
+          sql.enable = true;
+          svelte.enable = false;
+          tailwind.enable = false;
+          ts.enable = true;
+          typst.enable = false;
+          vala.enable = false;
+          yaml.enable = true;
+          zig.enable = true;
+
+          # Nim LSP is broken on Darwin and therefore
+          # should be disabled by default. Users may still enable
+          # `vim.languages.vim` to enable it, this does not restrict
+          # that.
+          # See: <https://github.com/PMunch/nimlsp/issues/178#issue-2128106096>
+          nim.enable = false;
+        };
+
+        visuals = {
+          nvim-scrollbar.enable = false;
+          nvim-web-devicons.enable = true;
+          nvim-cursorline.enable = true;
+          cinnamon-nvim.enable = true;
+          fidget-nvim.enable = true;
+
+          highlight-undo.enable = true;
+          indent-blankline.enable = true;
+
+          # Fun
+          cellular-automaton.enable = false;
+        };
+
+        statusline = {
+          lualine = {
+            enable = true;
+            theme = "catppuccin";
+          };
+        };
+
+        theme = {
+          enable = true;
+          name = "catppuccin";
+          style = "mocha";
+          transparent = false;
+        };
+
+        utility = {
+          oil-nvim.enable = true;
+        };
+
+        autopairs.nvim-autopairs.enable = true;
+
+        autocomplete.nvim-cmp.enable = true;
+        snippets.luasnip.enable = true;
+
+        telescope = {
+          enable = true;
+        };
+      };
+    };
   };
 
   # Let Home Manager install and manage itself.
