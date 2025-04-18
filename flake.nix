5520925fb4a7d36950f5bf7a2d4fb0e6264632af
sknutsen <sondreknutsen1@gmail.com>
2025-04-18 13:39:40 +0200

    nvim module

--- flake.nix
+++ flake.nix
@@ -9,39 +9,42 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    
+
     nvf = {
       url = "github:notashelf/nvf";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }@inputs:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-      # pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      homeConfigurations."zdk" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-
-	extraSpecialArgs = {
-	  inherit inputs;
-          rootPath = ./.;
-	};
-
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
-        modules = [
-          inputs.nvf.homeManagerModules.default
-
-	  ./home.nix
-	];
-
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
-      };
+  outputs = {
+    nixpkgs,
+    home-manager,
+    ...
+  } @ inputs: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
     };
+    # pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    homeConfigurations."zdk" = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+
+      extraSpecialArgs = {
+        inherit inputs;
+      };
+
+      # Specify your home configuration modules here, for example,
+      # the path to your home.nix.
+      modules = [
+        inputs.nvf.homeManagerModules.default
+
+        ./nvim/nvim.nix
+        ./home.nix
+      ];
+
+      # Optionally use extraSpecialArgs
+      # to pass through arguments to home.nix
+    };
+  };
 }
